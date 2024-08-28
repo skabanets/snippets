9d22c7fbb798769bd694831b705505850d9d0579
Sergey Kabanets <skabanets97@gmail.com>
2024-08-28 17:38:58 +0300

    Add fetching snippets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import { db } from "@/db";
 
-export default function Home() {
-  return <div>Home Page</div>;
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>;
+  });
+
+  return <div>{renderedSnippets}</div>;
 }
