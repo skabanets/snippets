34411bfccfa865391a1f6cd0682fc964cdddfd0c
Sergey Kabanets <skabanets97@gmail.com>
2024-08-28 18:44:08 +0300

    Update hone page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
+import Link from "next/link";
 import { db } from "@/db";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        key={snippet.id}
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between items-center p-2 border rounded"
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className="flex my-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href={"/snippets/new"} className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
