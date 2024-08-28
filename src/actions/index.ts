a2d9b1c187daf8f154c42442b94dfd956d39b020
Sergey Kabanets <skabanets97@gmail.com>
2024-08-28 21:10:25 +0300

    Add deleteSnippet action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -5,6 +5,10 @@ import { db } from "@/db";
 
 export async function editSnippet(id: number, code: string) {
   await db.snippet.update({ where: { id }, data: { code } });
-
   redirect(`/snippets/${id}`);
+}
+
+export async function deleteSnippet(id: number) {
+  await db.snippet.delete({ where: { id } });
+  redirect("/");
 }
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { db } from "@/db";
+import * as actions from "@/actions";
 
 interface SnippetShowPageProps {
   params: {
@@ -15,9 +16,10 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
   const snippet = await db.snippet.findFirst({
     where: { id },
   });
-  console.log(props);
 
   if (!snippet) notFound();
+
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
 
   return (
     <div>
@@ -30,7 +32,9 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
           >
             Edit
           </Link>
-          <button className="p-2 border rounded">Delete</button>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded">Delete</button>
+          </form>
         </div>
       </div>
       <pre className="p-3 border rounded bg-gray-200 border-gray-200">
